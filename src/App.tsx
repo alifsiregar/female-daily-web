761560fdef9f9627a93d6f102ea0490d1b45ed19
Alif Siregar <alifsiregar21@gmail.com>
2021-08-18 10:02:02 +0700

    add requests to app and import components (WIP)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect  } from 'react';
 import { GlobalStyle } from './styles';
+import {
+  Header,
+  Categories,
+  Hero,
+  Editor
+} from './components';
+import { getLandingPageData } from './services';
 
 function App() {
+
+  const [editorInfo, setEditorInfo] = useState<{
+    editor: string,
+    role: string,
+    product: {
+      name: string,
+      rating: number,
+      description: string,
+      image: string
+    }
+  }[]>([]);
+  const [articlesInfo, setArticlesInfo] = useState<{}[]>([]);
+  const [reviewInfo, setReviewInfo] = useState<{}[]>([]);
+
+  const getData = async () : Promise<void> => {
+    await getLandingPageData().then((res:any) => {
+      setEditorInfo(res["editor's choice"]);
+      setArticlesInfo(res["latest articles"]);
+      setReviewInfo(res["latest review"]);
+    });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <>
       <GlobalStyle />
-      <span>Hello World</span>
+      <Header />
+      <Categories />
+      <Hero />
+      <Editor editorInfo={editorInfo} />
     </>
   );
 }
