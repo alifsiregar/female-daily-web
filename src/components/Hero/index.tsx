6e5c761788cb59e71dc58d2e3c1ad207c79e723a
Alif Siregar <alifsiregar21@gmail.com>
2021-08-18 09:59:22 +0700

    completed hero component

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+    Container
+} from './styles';
+import {
+    TopFrame,
+    Billboard
+} from '../Advertisements';
+
+const Hero = () => {
+    return (
+        <Container>
+            <TopFrame />
+            <Billboard />
+        </Container>
+    )
+}
+
+export default Hero
---
+++ src/components/Hero/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    margin-top: 20px;
+    margin-bottom: 48px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
