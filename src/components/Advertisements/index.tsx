bff93b251fc4eb570829d787ca41389cc39e0302
Alif Siregar <alifsiregar21@gmail.com>
2021-08-18 09:59:39 +0700

    add advertisements components (WIP)

---
+++ src/components/Advertisements/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+    TopFrameContainer,
+    BillboardContainer
+} from './styles';
+
+export const TopFrame = () => {
+    return (
+        <TopFrameContainer>
+            <h2>
+                Top Frame 970x50
+            </h2>
+        </TopFrameContainer>
+    )
+}
+
+export const Billboard = () => {
+    return (
+        <BillboardContainer>
+            <h2>
+                Billboard 970x250
+            </h2>
+        </BillboardContainer>
+    )
+}
---
+++ src/components/Advertisements/styles.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const TopFrameContainer = styled.div`
+    width: 970px;
+    height: 50px;
+    background-color: #d8d8d8;
+    border: 0.1rem solid #979797;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    h2 {
+        color: #5a5a5a;
+    }
+`;
+
+export const BillboardContainer = styled.div`
+    width: 970px;
+    height: 250px;
+    margin-top: 32px;
+    background-color: #d8d8d8;
+    border: 0.1rem solid #979797;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    h2 {
+        color: #5a5a5a;
+    }
+`;
