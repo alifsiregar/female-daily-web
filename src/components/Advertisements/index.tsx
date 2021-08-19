1e5cd6fdd97007c30deeb856351986e584fa71c4
Alif Siregar <alifsiregar21@gmail.com>
2021-08-19 08:52:15 +0700

    completed advertisements

--- src/components/Advertisements/index.tsx
+++ src/components/Advertisements/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import {
     TopFrameContainer,
-    BillboardContainer
+    BillboardHorizontalContainer,
+    MR2Container,
+    BottomFrameContainer
 } from './styles';
 
 export const TopFrame = () => {
@@ -16,10 +18,40 @@ export const TopFrame = () => {
 
 export const Billboard = () => {
     return (
-        <BillboardContainer>
+        <BillboardHorizontalContainer>
             <h2>
                 Billboard 970x250
             </h2>
-        </BillboardContainer>
+        </BillboardHorizontalContainer>
+    )
+}
+
+export const Horizontal = () => {
+    return (
+        <BillboardHorizontalContainer>
+            <h2>
+                Horizontal 970x250 <br /> Internal Campaign Only
+            </h2>
+        </BillboardHorizontalContainer>
+    )
+}
+
+export const MR2 = () => {
+    return (
+        <MR2Container>
+            <h2>
+                MR2<br />300x250
+            </h2>
+        </MR2Container>
+    )
+}
+
+export const BottomFrame = () => {
+    return (
+        <BottomFrameContainer>
+            <h2>
+                Bottom Frame 970x50, 468x60, 320x50
+            </h2>
+        </BottomFrameContainer>
     )
 }
--- src/components/Advertisements/styles.ts
+++ src/components/Advertisements/styles.ts
@@ -14,7 +14,7 @@ export const TopFrameContainer = styled.div`
     }
 `;
 
-export const BillboardContainer = styled.div`
+export const BillboardHorizontalContainer = styled.div`
     width: 970px;
     height: 250px;
     margin-top: 32px;
@@ -23,6 +23,38 @@ export const BillboardContainer = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    text-align: center;
+    
+    h2 {
+        color: #5a5a5a;
+    }
+`;
+
+export const MR2Container = styled.div`
+    width: 300px;
+    height: 250px;
+    background-color: #d8d8d8;
+    border: 0.1rem solid #979797;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    
+    h2 {
+        color: #5a5a5a;
+    }
+`;
+
+export const BottomFrameContainer = styled.div`
+margin-top: 32px;
+    width: 970px;
+    height: 50px;
+    background-color: #d8d8d8;
+    border: 0.1rem solid #979797;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
     
     h2 {
         color: #5a5a5a;
